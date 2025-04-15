db2749980187c0063d63fe198b1d6406c3933aef
UnstoppableMoron <cte-owen.baker@sllboces.org>
2025-04-15 17:52:33 +0000

    making progress

---
+++ scripts.js
@@ -0,0 +1,16 @@
+//Collapse and expand information on click
+$(".skillsTrigger").click(function(){
+    $("#skillsText").slideToggle();
+});
+
+$(".experienceTrigger").click(function(){
+    $("#experienceText").slideToggle();
+});
+
+$(".educationTrigger").click(function(){
+    $("#educationText").slideToggle();
+});
+
+$(".referencesTrigger").click(function(){
+    $("#referencesText").slideToggle();
+});
